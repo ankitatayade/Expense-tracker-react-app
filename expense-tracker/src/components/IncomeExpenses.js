fc45c2f13b9be55a2e68847b9946fa47a9b1c48b
ankita <ankita.tayde94@gmail.com>
2024-04-09 14:05:57 +0530

    react project

---
+++ expense-tracker/src/components/IncomeExpenses.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const IncomeExpense = () => { 
+
+    return (
+        <div className="inc-exp-container">
+        <div>
+          <h4>Income</h4>
+          <p  className="money plus">+Rs.0.00</p>
+        </div>
+        <div>
+          <h4>Expense</h4>
+          <p className="money minus">-Rs.0.00</p>
+        </div>
+      </div>
+    )
+}
