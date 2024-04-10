53556968912700ea9e082bff8dece0553bc3d519
ankita <ankita.tayde94@gmail.com>
2024-04-10 10:28:11 +0530

    react project

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { Header} from './components/Header'; 
 import { Balance } from './components/Balance';
 import { IncomeExpense } from './components/IncomeExpenses';
-import { TransactionList } from './components/TrasactionList';
+import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
 import './App.css';
+import { GlobalProvider } from './context/Globalstate';
+
 
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header/>
       <div className = "container"> 
       <Balance/>
@@ -17,7 +19,7 @@ function App() {
       <TransactionList/>
       <AddTransaction/>
         </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
